61f077d3f3efb0911081180e0cc3429e3aeb6d12
JasonPunyon <jason.punyon@gmail.com>
2011-11-27 09:49:46 -0500

    Added RedisSignalBus and MessageStore implementations.
    Setup package restore.

---
+++ SignalR.Redis/RedisMessageStore.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using BookSleeve;
+using ProtoBuf;
+
+namespace SignalR.Redis
+{
+    public class RedisMessageStore : IMessageStore
+    {
+        private const string MessageIdKey = "SignalR.MessageId";
+
+        private static object _connectionLock = new object();
+        private static RedisConnection _redisConnection;
+
+        private int _database;
+        private JavaScriptSerializer _serializer; 
+
+        public RedisMessageStore(int database, string host = "localhost", int port = 6379, string password = null) 
+            : this (new RedisConnection(host, port, password: password), database) { }
+
+        private RedisMessageStore(RedisConnection redisConnection, int database)
+        {
+            if (_redisConnection == null)
+            {
+                lock (_connectionLock)
+                {
+                    if (_redisConnection == null)
+                    {
+                        _redisConnection = redisConnection;
+                        _redisConnection.Open();
+                    }
+                }
+            }
+            
+            _database = database;
+            _serializer = new JavaScriptSerializer();
+        }
+
+        public Task<long?> GetLastId()
+        {
+            return _redisConnection.Strings.GetString(_database, MessageIdKey)
+                .ContinueWith(t =>
+                                  {
+                                      long id;
+                                      Int64.TryParse(t.Result, out id);
+                                      return (long?)id;
+                                  });
+        }
+
+        public Task Save(string key, object value)
+        {
+            return _redisConnection.Strings.Increment(_database, MessageIdKey)
+                .ContinueWith(t =>
+                                  {
+                                      var message = new ProtoMessage
+                                                        {
+                                                            Created = DateTime.Now,
+                                                            SignalKey = key,
+                                                            Id = t.Result,
+                                                            Value = _serializer.Serialize(value)
+                                                        };
+                                      _redisConnection.SortedSets.Add(_database,
+                                                                      key,
+                                                                      message.Serialize(),
+                                                                      message.Id);
+                                  });
+        }
+
+        public Task<IEnumerable<Message>> GetAllSince(string key, long id)
+        {
+            return _redisConnection.SortedSets.Range(_database,
+                                                     key,
+                                                     (double) id,
+                                                     (double) long.MaxValue,
+                                                     minInclusive: false)
+                .ContinueWith(t =>
+                              t.Result
+                                  .Select(o => ProtoMessage.Deserialize(o.Key))
+                                  .Select(o => new Message(o.SignalKey,
+                                                           o.Id,
+                                                           o.SignalKey.EndsWith("__SIGNALRCOMMAND__")
+                                                               ? o.Value
+                                                               : _serializer.DeserializeObject(o.Value),
+                                                           o.Created)));
+        }
+    }
+
+    [ProtoContract]
+    public class ProtoMessage
+    {
+        public static ProtoMessage Deserialize(byte[] bytes)
+        {
+            using (var ms = new MemoryStream(bytes))
+            {
+                return Serializer.Deserialize<ProtoMessage>(ms);
+            }
+        }
+
+        public byte[] Serialize()
+        {
+            using (var ms = new MemoryStream())
+            {
+                Serializer.Serialize(ms, this);
+                return ms.ToArray();
+            }
+        }
+
+        [ProtoMember(1)]
+        public string SignalKey { get; set; }
+
+        [ProtoMember(2)]
+        public string Value { get; set; }
+
+        [ProtoMember(3)]
+        public long Id { get; set; }
+
+        [ProtoMember(4)]
+        public DateTime Created { get; set; }
+    }
+}
---
+++ SignalR.Redis/RedisSignalBus.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BookSleeve;
+
+namespace SignalR.Redis
+{
+    public class RedisSignalBus : ISignalBus
+    {
+        private static object _connectionLock = new object();
+        private static RedisConnection _redisConnection;
+        private static RedisSubscriberConnection _subscriberConnection;
+        private static ConcurrentDictionary<string, SafeSet<EventHandler<SignaledEventArgs>>> _handlers;
+
+        public RedisSignalBus(string host = "localhost", int port = 6379, string password = null) : this(new RedisConnection(host, port, password: password))
+        {
+        }
+
+        private RedisSignalBus(RedisConnection redisConnection)
+        {
+            if (_redisConnection == null)
+            {
+                lock (_connectionLock)
+                {
+                    if (_redisConnection == null)
+                    {
+                        _handlers = new ConcurrentDictionary<string, SafeSet<EventHandler<SignaledEventArgs>>>();
+                        _redisConnection = redisConnection;
+                        _redisConnection.Open();
+                        _subscriberConnection = _redisConnection.GetOpenSubscriberChannel();
+                        _subscriberConnection.MessageReceived += OnRedisMessageReceived;
+                    }
+                }
+            }
+        }
+
+        private void OnRedisMessageReceived(string eventKey, byte[] messageBytes)
+        {
+            OnSignaled(eventKey);
+        }
+
+        private void OnSignaled(string eventKey)
+        {
+            //Get handlers for this event...
+            SafeSet<EventHandler<SignaledEventArgs>> handlersForEvent;
+            if (_handlers.TryGetValue(eventKey, out handlersForEvent))
+            {
+                Parallel.ForEach(handlersForEvent.GetSnapshot(), h => h(this, new SignaledEventArgs(eventKey)));
+            }
+        }
+
+        public Task Signal(string eventKey)
+        {
+            //Run our local event handlers for the key and publish the event to anyone listening.
+            return
+                Task.Factory.StartNew(() =>
+                {
+                    OnSignaled(eventKey);
+                    _redisConnection.Publish(eventKey, "", true).Wait();
+                });
+        }
+
+        public void AddHandler(string eventKey, EventHandler<SignaledEventArgs> handler)
+        {
+            //Get or create the bag of handlers for the event named {eventKey}
+            var eventHandlersForKey = _handlers.GetOrAdd(eventKey, new SafeSet<EventHandler<SignaledEventArgs>>());
+
+            //Determine whether we need to create a new redis subscription.
+            var newRedisSubscriptionRequired = eventHandlersForKey.GetSnapshot().Count() == 0;
+
+            //Add the handler to the bag and create the redis subscription if necessary
+            eventHandlersForKey.Add(handler);
+            if (newRedisSubscriptionRequired)
+            {
+                _subscriberConnection.Subscribe(eventKey);
+            }
+        }
+
+        public void RemoveHandler(string eventKey, EventHandler<SignaledEventArgs> handler)
+        {
+            SafeSet<EventHandler<SignaledEventArgs>> eventHandlersForKey;
+            if (_handlers.TryGetValue(eventKey, out eventHandlersForKey))
+            {
+                //We had a subscription, remove the handler.
+                eventHandlersForKey.Remove(handler);
+
+                //If there are no more handlers for this event unsubscribe our redis connection.
+                if (eventHandlersForKey.GetSnapshot().Count() == 0)
+                {
+                    _handlers.TryRemove(eventKey, out eventHandlersForKey);
+                    _subscriberConnection.Unsubscribe(eventKey);
+                }
+            }
+        }
+    }
+}
